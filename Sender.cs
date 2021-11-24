8750761a0d10f36bd56abe6428380b5871f4b003
DavidSciMeow <davidtennant405@outlook.com>
2021-11-24 16:03:25 +0800

    2.0.0

--- Sender.cs
+++ Sender.cs
@@ -1,40 +1,103 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* 本文上下文定义了消息事件的引起者,
+ * 根据消息方案查询引起者, 本类类内继承顺序不允许调换.
+ * ------------------------------
+ * 
+ */
 
 namespace MeowMiraiLib.Msg.Sender
 {
+    /// <summary>
+    /// 消息引起者定义
+    /// </summary>
     public class Sender
     {
+        /// <summary>
+        /// 消息位定义
+        /// </summary>
         public long id;
     }
+    /// <summary>
+    /// 好友信息好友句柄
+    /// </summary>
     public class FriendMessageSender : Sender
     {
+        /// <summary>
+        /// 好友昵称
+        /// </summary>
         public string nickname;
+        /// <summary>
+        /// 好友备注
+        /// </summary>
         public string remark;
     }
+    /// <summary>
+    /// 群信息句柄
+    /// </summary>
     public class GroupMessageSender : Sender
     {
+        /// <summary>
+        /// 成员名称
+        /// </summary>
         public string memberName;
+        /// <summary>
+        /// 成员头衔
+        /// </summary>
         public string specialTitle;
+        /// <summary>
+        /// 成员群权限
+        /// </summary>
         public string permission;
+        /// <summary>
+        /// 入群时间
+        /// </summary>
         public long joinTimestamp;
+        /// <summary>
+        /// 上次发言时间
+        /// </summary>
         public long lastSpeakTimestamp;
+        /// <summary>
+        /// 剩余禁言时间
+        /// </summary>
         public long muteTimeRemaining;
+        /// <summary>
+        /// 群信息
+        /// </summary>
         public Group group;
+        /// <summary>
+        /// 群信息句柄群信息类
+        /// </summary>
         public class Group
         {
+            /// <summary>
+            /// 群号
+            /// </summary>
             public long id;
+            /// <summary>
+            /// 群名
+            /// </summary>
             public string name;
+            /// <summary>
+            /// 在群里的权限
+            /// </summary>
             public string permission;
         }
     }
+    /// <summary>
+    /// 临时信息句柄
+    /// </summary>
     public class TempMessageSender : GroupMessageSender { }
+    /// <summary>
+    /// 陌生人信息句柄
+    /// </summary>
     public class StrangerMessageSender : FriendMessageSender { }
+    /// <summary>
+    /// 其他客户端信息句柄
+    /// </summary>
     public class OtherClientMessageSender : Sender
     {
+        /// <summary>
+        /// 平台句柄
+        /// </summary>
         public string platform;
     }
 }
